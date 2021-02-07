908339671ab5d65be1ebfdd2fd84438d6ea96403
Fumiya Sakai <just1factory@gmail.com>
2021-02-07 20:17:12 +0900

    ファイル整理＆基盤部分整備

--- UIStructuresWithRxSwift/ViewController/MainViewController.swift
+++ UIStructuresWithRxSwift/ViewController/MainViewController.swift
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
+
+    // MARK: - Override
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
